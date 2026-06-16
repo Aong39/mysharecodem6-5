0021d7922f85b3b311aaca75c7443bb1ca928d6d
Aong39 <164354@wsk.ac.th>
2026-06-16 04:55:36 +0000

    first02.py

--- first01.py
+++
@@ -1,6 +0,0 @@
-print("โปรแกรมคำนวณพื้นที่ของสามเหลี่ยม")
-h=float(input("Enter hight:"))
-b=float(input("Enter base:"))
-area=0.5*h*b
-print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
-print("จัดทำโดย นางสาวออง ตาย้อย เลขที่39")
